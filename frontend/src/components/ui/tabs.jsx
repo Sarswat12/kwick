35b5224ebf8c4232b4322821d3cf409443afc0be
shank <shank@users.noreply.github.com>
2025-12-27 14:30:02 +0530

    Remove all versioned imports and aliases, fix React context errors for Vite production build

--- frontend/src/components/ui/tabs.jsx
+++ frontend/src/components/ui/tabs.jsx
@@ -1,6 +1,6 @@
 "use client";
 import * as React from "react";
-import * as TabsPrimitive from "@radix-ui/react-tabs@1.1.3";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
 import { cn } from "./utils";
 function Tabs({ className, ...props }) {
     return (<TabsPrimitive.Root data-slot="tabs" className={cn("flex flex-col gap-2", className)} {...props}/>);
